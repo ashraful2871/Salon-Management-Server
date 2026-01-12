04b9884096a53a0a1192af042a674feba78e9de9
ashraful2871 <ashrafulislam2758@gmail.com>
2026-01-12 23:11:26 +0600

    delete file

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,7 +1,7 @@
-import dotenv from 'dotenv';
-import path from 'path';
+import dotenv from "dotenv";
+import path from "path";
 
-dotenv.config({ path: path.join(process.cwd(), '.env') });
+dotenv.config({ path: path.join(process.cwd(), ".env") });
 
 interface Config {
   env: string | undefined;
@@ -19,16 +19,17 @@ interface Config {
     api_secret: string | undefined;
   };
 }
+//
 
 export default {
   env: process.env.NODE_ENV,
   port: process.env.PORT,
   database_url: process.env.DATABASE_URL,
   jwt: {
-    jwt_secret: process.env.JWT_SECRET || '',
-    expires_in: process.env.EXPIRES_IN || '1h',
-    refresh_token_secret: process.env.REFRESH_TOKEN_SECRET || '',
-    refresh_token_expires_in: process.env.REFRESH_TOKEN_EXPIRES_IN || '90d',
+    jwt_secret: process.env.JWT_SECRET || "",
+    expires_in: process.env.EXPIRES_IN || "1h",
+    refresh_token_secret: process.env.REFRESH_TOKEN_SECRET || "",
+    refresh_token_expires_in: process.env.REFRESH_TOKEN_EXPIRES_IN || "90d",
   },
   cloudinary: {
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
